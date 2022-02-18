a1374ab97fa58d10ab63a7466aec14e694afa6d5
Andres Hernandez <edanhebla1213@hotmail.com>
2022-02-18 12:13:10 -0500

    home components market

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { PagesRoutingModule } from './pages-routing.module';
+
+import { ButtonModule } from 'primeng/button';
+
 import { CalculatorComponent } from './calculator/calculator.component';
 import { UsersComponent } from './users/users.component';
-
-import { PagesRoutingModule } from './pages-routing.module';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [CalculatorComponent, UsersComponent, HomeComponent],
-  imports: [CommonModule, PagesRoutingModule],
+  imports: [CommonModule, PagesRoutingModule, SharedModule, ButtonModule],
 })
 export class PagesModule {}
