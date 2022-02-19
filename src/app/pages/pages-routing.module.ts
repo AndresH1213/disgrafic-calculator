2214583275d57341333b062d447423cf2b3a98d2
Andres Hernandez <edanhebla1213@hotmail.com>
2022-02-18 23:49:52 -0500

    footer responsive

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'calculator', component: CalculatorComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 import { PagesRoutingModule } from './pages-routing.module';
 
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { DropdownModule } from 'primeng/dropdown';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
 
 import { CalculatorComponent } from './calculator/calculator.component';
 import { UsersComponent } from './users/users.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [CalculatorComponent, UsersComponent, HomeComponent],
-  imports: [CommonModule, PagesRoutingModule, SharedModule, ButtonModule],
+  declarations: [CalculatorComponent, UsersComponent, HomeComponent, AboutComponent],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    SharedModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule,
+    DropdownModule,
+    CascadeSelectModule,
+  ],
 })
 export class PagesModule {}
