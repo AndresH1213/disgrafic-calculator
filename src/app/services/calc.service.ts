dfe94740a7cf375b6c09ed79617d735ea81e2ef2
Andres Hernandez <edanhebla1213@hotmail.com>
2022-02-21 01:26:26 -0500

    retrieving clients and material service

--- src/app/services/calc.service.ts
+++ src/app/services/calc.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalcService {
+  get headers() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        Accept: '*/*',
+      }),
+    };
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getMaterials() {
+    const url = `${baseUrl}/products`;
+    return this.http.get(url, this.headers);
+  }
+
+  getClients() {
+    const url = `${baseUrl}/clients`;
+    return this.http.get(url, this.headers);
+  }
 }
