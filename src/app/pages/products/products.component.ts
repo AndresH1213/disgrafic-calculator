8d0467107c5d6374efdd3fdac9698609c083b5b2
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-18 01:07:49 -0500

    images products carousel

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from 'src/app/services/photo.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  products: any[] = [];
+  responsiveOptions: any[] = [];
+  constructor(private photoService: PhotoService) {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+        numScroll: 3,
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+        numScroll: 2,
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1,
+      },
+    ];
   }
 
+  ngOnInit() {
+    this.products = this.photoService.getProducts();
+  }
 }
