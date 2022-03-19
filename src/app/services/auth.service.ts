922393373396867aa080cedb39134b798052e875
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-19 03:22:22 -0500

    auth first approach

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+const base_url = environment.baseUrl;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  get token() {
+    return localStorage.getItem('token') || '';
+  }
 
-  constructor() { }
+  get headers() {
+    return {
+      headers: {
+        Authorization: this.token,
+      },
+    };
+  }
+
+  saveLocalStorage(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  constructor(private http: HttpClient) {}
+
+  validateToken(token: string): Observable<boolean> {
+    return this.http
+      .get(`${base_url}/confirm/?token=${token}`, {
+        headers: {
+          'x-token': this.token,
+        },
+      })
+      .pipe(
+        map((data: any) => {
+          this.saveLocalStorage(data.token);
+
+          return true;
+        }),
+        catchError((err) => of(false))
+      );
+  }
 }
