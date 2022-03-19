a840b9850b954cfbb247e00b728fadf1b635e8fa
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-19 03:43:20 -0500

    shared nav clients removed

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,16 +9,20 @@ import { MenuItem } from 'primeng/api';
 })
 export class NavbarComponent implements OnInit {
   items: MenuItem[] = [];
-
-  constructor() {}
+  isAuth: boolean = false;
+  constructor(private auth: AuthService) {
+    this.isAuth = !!this.auth.token;
+  }
 
   ngOnInit(): void {
     this.items = [
       { label: 'Inicio', icon: 'pi pi-home', routerLink: '/' },
       { label: 'Productos', icon: 'pi pi-products', routerLink: '/products' },
       { label: 'Calculadora', icon: 'pi pi-plus', routerLink: '/calculator' },
-      { label: 'Usuarios', icon: 'pi pi-refresh', routerLink: '/users' },
       { label: 'Nosotros', icon: 'pi pi-download', routerLink: '/about' },
+      this.isAuth
+        ? { label: 'Usuarios', icon: 'pi pi-refresh', routerLink: '/users' }
+        : {},
     ];
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://qoiz5kadn7.execute-api.us-east-2.amazonaws.com',
 };
