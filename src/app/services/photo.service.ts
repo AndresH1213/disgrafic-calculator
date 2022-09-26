9bcf42d54cdb1c21c842ed6dc4b9c715ec9d7856
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-25 21:56:27 -0500

    delete image implmenetation service

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,122 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { getBaseUrl } from './utils';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PhotoService {
-  constructor() {}
+  baseUrl: string;
+  constructor(private http: HttpClient) {
+    this.baseUrl = getBaseUrl();
+  }
 
-  getProducts() {
-    return [
-      {
-        image: 'agendas.PNG',
-        alt: 'Imagen agenda personalizada',
-        name: 'Agenda Personalizada',
-        price: 5000,
-      },
-      {
-        image: 'agendas2.PNG',
-        alt: 'Imagen agenda personalizada',
-        name: 'Agenda Personalizada',
-        price: 5000,
-      },
-      {
-        image: 'agendas3.PNG',
-        alt: 'Imagen agenda personalizada',
-        name: 'Agenda Personalizada',
-        price: 5000,
-      },
-      {
-        image: 'album-fotos.PNG',
-        alt: 'Album de fotos personalizada',
-        name: 'Album de fotos',
-        price: 15000,
-      },
-      {
-        image: 'boton-personalizado.PNG',
-        alt: 'Imagen Boton personalizado',
-        name: 'Boton personalizado',
-        price: 2000,
-      },
-      {
-        image: 'boton-personalizado2.PNG',
-        alt: 'Imagen Boton personalizado',
-        name: 'Boton personalizado',
-        price: 2000,
-      },
-      {
-        image: 'camisas-personalizadas.PNG',
-        alt: 'Imagen Camisas Personalizadas',
-        name: 'Camisas Personalizadas',
-        price: 35000,
-      },
-      {
-        image: 'camisas-personalizadas2.PNG',
-        alt: 'Imagen Camisas Personalizadas',
-        name: 'Camisas Personalizadas',
-        price: 35000,
-      },
-      {
-        image: 'cojines.PNG',
-        alt: 'Imagen Cojines',
-        name: 'Cojines',
-        price: 35000,
-      },
-      {
-        image: 'cojines2.PNG',
-        alt: 'Imagen Cojines',
-        name: 'Cojines',
-        price: 35000,
-      },
-      {
-        image: 'cojines3.PNG',
-        alt: 'Imagen Cojin',
-        name: 'Cojines Personalizados',
-        price: 35000,
-      },
-      {
-        image: 'cuadros.PNG',
-        alt: 'Imagen Cuadros',
-        name: 'Cuadros',
-        price: 15000,
-      },
-      {
-        image: 'lapiceros.PNG',
-        alt: 'Imagen Lapiceros personalizados',
-        name: 'Lapiceros Personalizados',
-        price: 1500,
-      },
-      {
-        image: 'lapiceros2.PNG',
-        alt: 'Imagen Lapiceros personalizados',
-        name: 'Lapiceros Personalizados',
-        price: 1500,
-      },
-      {
-        image: 'llaveros.PNG',
-        alt: 'Imagen llaveros',
-        name: 'Llaveros',
-        price: 3500,
-      },
-      {
-        image: 'llaveros2.PNG',
-        alt: 'Imagen llaveros',
-        name: 'Llaveros',
-        price: 3500,
-      },
-      {
-        image: 'mug-personalizado.PNG',
-        alt: 'Imagen mug-personalizado',
-        name: 'mug-personalizado',
-        price: 12000,
-      },
-      {
-        image: 'mug-personalizado2.PNG',
-        alt: 'Imagen Mug Personalizado',
-        name: 'Mug Personalizado',
-        price: 12000,
-      },
-    ];
+  getPresignedUrls(image: any) {
+    const headers = new HttpHeaders().set('Accept', 'application/json');
+    const params = new HttpParams()
+      .set('name', image.name)
+      .set('mime', image.type);
+    const url = `${this.baseUrl}/image-sign`;
+    return this.http.get(url, { params, headers });
+  }
+
+  uploadfileAWSS3(fileUploadUrl: string, contentType: string, file: any) {
+    const headers = new HttpHeaders({
+      'Content-Type': contentType,
+      'x-amz-acl': 'public-read',
+    });
+    const req = new HttpRequest('PUT', fileUploadUrl, file, {
+      headers: headers,
+    });
+    return this.http.request(req);
+  }
+
+  deleteFileAWSS3(name: string) {
+    const url = `${this.baseUrl}/image/` + name;
+    return this.http.delete(url);
   }
 }
