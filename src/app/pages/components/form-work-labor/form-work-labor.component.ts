c0bcccd68c4e6253fc74badfa00335033b8fc3cb
Andres Hernandez <edanhebla1213@hotmail.com>
2022-09-27 23:58:34 -0500

    removing retrieving clients list logic from component

--- src/app/pages/components/form-work-labor/form-work-labor.component.ts
+++ src/app/pages/components/form-work-labor/form-work-labor.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CalcService } from '../../../services/calc.service';
 import { ClientsService } from '../../../services/clients.service';
+
+interface field {
+  name: string;
+  price: number;
+}
+
+interface percentage {
+  name: string;
+  value: string;
+}
 
 @Component({
   selector: 'app-form-work-labor',
@@ -8,76 +18,37 @@ import { ClientsService } from '../../../services/clients.service';
   styleUrls: ['./form-work-labor.component.css'],
 })
 export class FormWorkLaborComponent implements OnInit {
-  formFields: any = {
-    diseno: {
-      label: 'Diseño',
-      precio: 0,
-    },
-    numerada: {
-      label: 'Numerada',
-      precio: 0,
-    },
-    perforada: {
-      label: 'Perforada',
-      precio: 0,
-    },
-    troquelada: {
-      label: 'Troquelada',
-      precio: 0,
-    },
-    troquel: {
-      label: 'Troquel',
-      precio: 0,
-    },
-    encuadernacion: {
-      label: 'Encuadernación',
-      precio: 0,
-    },
-    plastificado: {
-      label: 'Plastificado',
-      precio: 0,
-    },
-    reservauv: {
-      label: 'Reserva UV',
-      precio: 0,
-    },
-    empaqueEnvio: {
-      label: 'Empaque y Envio',
-      precio: 0,
-    },
-  };
-  selectedClient = '';
+  @Output() formHandWork: EventEmitter<number> = new EventEmitter();
+  @Output() percentage: EventEmitter<string> = new EventEmitter();
+
+  public percentages: percentage[] = [];
+  private labelFields: string[] = [];
+  formFields: field[] = [];
+
   value6: number | undefined;
-  workforceFields = [
-    'diseno',
-    'numerada',
-    'perforada',
-    'troquelada',
-    'troquel',
-    'encuadernacion',
-    'plastificado',
-    'reservauv',
-    'empaqueEnvio',
-  ];
-
-  porcentaje: number | undefined;
-
-  clients: any = [];
 
   PorcetajeGanacia: number | undefined;
-  constructor(
-    private calcService: CalcService,
-    private clientsService: ClientsService
-  ) {}
+  constructor(private calcService: CalcService) {
+    this.loadLabelField();
+    this.formFields = this.loadFormFields();
+  }
 
   ngOnInit(): void {
-    this.clientsService.getClients().subscribe((resp: any) => {
-      if (resp.ok) {
-        const { results } = resp;
-        this.clients = results.Items;
-        // console.log(this.clients);
-      }
-    });
+    this.loadPercentanges();
+  }
+
+  loadPercentanges() {
+    this.percentages = this.calcService.getPercentages();
+  }
+
+  loadLabelField() {
+    this.labelFields = this.calcService.getLabelFieldsHandWork();
+  }
+
+  loadFormFields() {
+    return new Array(this.labelFields.length)
+      .fill(undefined)
+      .map((_, i) => ({ name: this.labelFields[i], price: 0 }));
   }
 
   calculate() {
