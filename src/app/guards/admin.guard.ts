42409b8baf418534e845310f798982f59a39056b
Andres Hernandez <edanhebla1213@hotmail.com>
2022-10-03 22:29:27 -0500

    admin guard routes when is unauthenticated

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,15 +1,42 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  Router,
+  UrlSegment,
+  Route,
+} from '@angular/router';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    return this.authService.validateToken().pipe(
+      tap((isAuth) => {
+        if (!isAuth) {
+          this.router.navigateByUrl('/auth');
+        }
+      })
+    );
   }
-  
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.authService.validateToken().pipe(
+      tap((isAuth) => {
+        if (!isAuth) {
+          this.router.navigateByUrl('/auth');
+        }
+      })
+    );
+  }
 }
