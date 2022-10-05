d36168572b6a481b8f9c3055e16e3161c9a2f7a8
Andres Hernandez <edanhebla1213@hotmail.com>
2022-10-04 21:43:55 -0500

    utils funcionts provides headers and environment vars

--- src/app/services/utils.ts
+++ src/app/services/utils.ts
@@ -1,11 +1,17 @@
 import { environment } from 'src/environments/environment';
 
-export function getHeaders() {
-  return {
-    headers: {},
-  };
-}
-
 export function getBaseUrl() {
   return environment.baseUrl;
+}
+
+export function getToken() {
+  return localStorage.getItem('dis-token') || '';
+}
+
+export function getHeaders() {
+  return {
+    headers: {
+      Authorization: 'Bearer ' + getToken(),
+    },
+  };
 }
